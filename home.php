d0178aba043728ca48b3540abe6f3606d73ac5e3
gaurav211 <gaurav.tomar66@gmail.com>
2017-04-03 16:50:02 +0530

    insert new home file

--- home.php
+++ home.php
@@ -6,6 +6,14 @@ template name: Home page
 ****/
 get_header(); ?>
 
-<?php echo "llllllllllllpoooooooooooooooooooooooooooooooooooooooooooooooooo";?>
 
-<?php get_footer(); ?>
+	<!--slider part start from here-->
+	<section>
+		<div class="slider">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/banner.jpg" alt="Banner" class="img-responsive1">
+		</div>
+	</section>
+	<!--slider end here-->
+	
+
+<?php get_footer(); ?>>>>>>>
